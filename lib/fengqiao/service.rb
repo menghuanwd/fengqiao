3e61ade31a3a0cdeda8fa23628f2d561ccc61165
menghuanwd <651019063@qq.com>
2019-07-26 15:38:49 +0800

    更新路由测试

--- lib/fengqiao/service.rb
+++ lib/fengqiao/service.rb
@@ -32,11 +32,26 @@ module Fengqiao
       )
     end
 
-    def route(orderid)
+    # 路由查询接口支持三种查询方式:
+    # 根据通过丰桥接口下单的订单号查询,系统校验信息匹配将返回对应运单路由信息。
+    # 根据运单号+月结卡号(需与传入的顾客编码绑定)查询,系统校验信息归属关系正确将返回对应运单路由信息。
+    # 根据运单号+运单对应的收寄人任一方电话号码后4位(参数check_phoneNo中传入)查询,系统校验信息匹配将返回对应运单路由信息。
+
+    # tracking_type	Number(2)	否	1	查询号类别:
+    # 1:根据顺丰运单号查询,order节点中tracking_number将被当作顺丰运单号处理
+    # 2:根据客户订单号查询,order节点中tracking_number将被当作客户订单号处理
+    # 3:逆向单,根据客户原始订单号查询,order节点中tracking_number将被当作逆向单原始订单号处理
+
+    # 2	tracking_number	String(4000)	是
+    # 查询号:
+    # 如果tracking_type=1,则此值为顺丰运单号
+    # 如果tracking_type=2,则此值为客户订单号
+    # 如果tracking_type=3,则此值为逆向单原始订单号
+    # 如果有多个单号,以逗号分隔,如"123,124,125"。
+    def route(tracking_type="1", tracking_number)
       package('RouteService', 'RouteRequest', 
-        'tracking_type' => '2',
-        'method_type' => '1',
-        'tracking_number' => orderid
+        'tracking_type' => tracking_type,
+        'tracking_number' => tracking_number
       )
     end
 
--- test/fengqiao_test.rb
+++ test/fengqiao_test.rb
@@ -1,11 +1,39 @@
 require "test_helper"
 
 class FengqiaoTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Fengqiao::VERSION
+
+  def setup
+    Fengqiao.code = ''
+    Fengqiao.check_word = ''
+    Fengqiao.custid = ''
+    @tracking_number = ''
+    @err_tracking_number = ''
   end
 
-  def test_it_does_something_useful
-    assert false
+  # <?xml version='1.0' encoding='UTF-8'?><Response service='RouteService'>
+  # <Head>OK</Head>
+  # <Body>
+  # <RouteResponse mailno='SF1010575444025'>
+  # <Route accept_address='上海市' accept_time='2019-07-26 11:25:59' opcode='54' remark='顺丰速运 已收取快件'/>
+  # <Route accept_address='上海市' accept_time='2019-07-26 11:46:46' opcode='50' remark='顺丰速运 已收取快件'/>
+  # <Route accept_address='上海市' accept_time='2019-07-26 13:01:08' opcode='30' remark='快件在【上海嘉定真源营业部】已装车,准备发往 【上海普陀千阳营业点】'/>
+  # <Route accept_address='上海市' accept_time='2019-07-26 13:03:20' opcode='36' remark='快件已发车'/>
+  # <Route accept_address='上海市' accept_time='2019-07-26 14:26:00' opcode='31' remark='快件到达 【上海虹桥同城集散点】'/>
+  # <Route accept_address='上海市' accept_time='2019-07-26 14:26:00' opcode='30' remark='快件在【上海虹桥同城集散点】已装车,准备发往 【上海闵行同城集散点】'/>
+  # <Route accept_address='上海市' accept_time='2019-07-26 14:41:19' opcode='36' remark='快件已发车'/>
+  # </RouteResponse>
+  # </Body>
+  # </Response>
+  def test_query_route
+    res_doc = Fengqiao::Service.new.route(@tracking_number)
+    
+    assert_equal res_doc.root.children[0].text, 'OK'
+  end
+
+  # <?xml version='1.0' encoding='UTF-8'?><Response service='RouteService'><Head>OK</Head><Body/></Response>
+  def test_err_query_route
+    res_doc = Fengqiao::Service.new.route(@err_tracking_number)
+    
+    assert_equal res_doc.root.children[1].children, []
   end
 end
